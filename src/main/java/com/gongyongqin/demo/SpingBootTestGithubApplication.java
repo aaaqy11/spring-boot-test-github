2dabe9a7912b48eb9de6c41ed674be6750be1b8d
aaaqy11 <928427598@qq.com>
2019-03-19 10:08:48 +0800

    Initial commit 描述

---
+++ src/main/java/com/gongyongqin/demo/SpingBootTestGithubApplication.java
@@ -0,0 +1,13 @@
+package com.gongyongqin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootTestGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootTestGithubApplication.class, args);
+	}
+
+}
